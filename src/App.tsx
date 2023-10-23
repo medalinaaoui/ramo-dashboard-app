e47867cc967d78320dd5b3ec08501e319d1bb2d0
Med Ali Naaoui <chayga00@gmail.com>
2023-10-23 21:34:48 +0100

    ready to go

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio accusantium
+      ipsum nam, et illo voluptas voluptates unde amet deserunt culpa, fuga sed
+      reiciendis aliquam ratione facilis necessitatibus quidem non quam.
+    </div>
+  );
 };
 export default App;
